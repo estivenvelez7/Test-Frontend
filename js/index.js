a2a5bdb1f0614fa44d2bccacaa9a10a9a38104b4
estivenvelez7 <61256108+estivenvelez7@users.noreply.github.com>
2021-06-10 21:07:09 -0500

    The test was uploaded

---
+++ js/index.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  $(".slideshow").slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    adaptiveHeight: true,
+    centerMode: true,
+    arrows: false
+  });
+});
